82b6ffcab1bcd81075ebc7f0ec31024b8f18e51d
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-18 14:44:47 +0200

    Remove documentation

--- js/project.js
+++ js/project.js
@@ -1,7 +1,6 @@
 var numbers = [2, 6, 11, 5, 9, 8, 3, 46, 4];
 
 numbers.forEach((function(value, index, array) {
-  // To log function
   var lastValue = array[array.length - 1];
 
   if (lastValue === value) {
